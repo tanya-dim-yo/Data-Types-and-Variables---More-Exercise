3950cf494ec860a6ca20cb6c0258ad608e5f2c09
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-03 05:16:30 +0300

    Add files via upload

---
+++ 01. Data Type Finder/Program.cs
@@ -0,0 +1,25 @@
+﻿string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if (int.TryParse(input, out int numberInt))
+    {
+        Console.WriteLine($"{input} is integer type");
+    }
+    else if (float.TryParse(input, out float numberFloat))
+    {
+        Console.WriteLine($"{input} is floating point type");
+    }
+    else if (char.TryParse(input, out char symbol))
+    {
+        Console.WriteLine($"{input} is character type");
+    }
+    else if (bool.TryParse(input, out Boolean value))
+    {
+        Console.WriteLine($"{input} is boolean type");
+    }
+    else
+    {
+        Console.WriteLine($"{input} is string type");
+    }
+}
