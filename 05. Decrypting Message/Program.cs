269d9739d8f67f77bca80145b17a1c2102e96cb8
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-13 03:55:58 +0300

    Add files via upload

---
+++ 05. Decrypting Message/Program.cs
@@ -0,0 +1,16 @@
+﻿int key = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+char[] decryptedChars = new char[n];
+
+for (int i = 0; i < n; i++)
+{
+    char currentCharacter = char.Parse(Console.ReadLine());
+    int decryptedCharInt = (int)(currentCharacter) + key;
+    char decryptedChar = (char)decryptedCharInt;
+    decryptedChars[i] = decryptedChar;
+}
+
+for (int index = 0; index < decryptedChars.Length; index++)
+{
+    Console.Write(decryptedChars[index]);
+}
