a7825b9108b50b0a0e26f8a63bcfdf84504b73d8
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-13 03:57:00 +0300

    Add files via upload

---
+++ 06. Balanced Brackets/Program.cs
@@ -0,0 +1,38 @@
+﻿int n = int.Parse(Console.ReadLine());
+int openingBrackets = 0;
+int closingBrackets = 0;
+string lastString = "";
+bool flag = false;
+
+for (int i = 1; i <= n; i++)
+{
+    string currentString = Console.ReadLine();
+
+    if (currentString == lastString)
+    {
+        flag = true;
+    }
+
+    if (currentString == "(")
+    {
+        openingBrackets++;
+    }
+    else if (currentString == ")")
+    {
+        closingBrackets++;
+    }
+
+    if (currentString == "(" || currentString == ")")
+    {
+        lastString = currentString;
+    }
+}
+
+if (flag || openingBrackets != closingBrackets)
+{
+    Console.WriteLine("UNBALANCED");
+}
+else
+{
+    Console.WriteLine("BALANCED");
+}
