1508f4628605db5715122a7714d7e9b88a3339c6
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-13 03:54:40 +0300

    Add files via upload

---
+++ 03. Floating Equality/Program.cs
@@ -0,0 +1,14 @@
+﻿double number1 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine());
+double eps = 0.000001;
+
+double difference = Math.Abs(number1 - number2);
+
+if (difference >= eps)
+{
+    Console.WriteLine("False");
+}
+else
+{
+    Console.WriteLine("True");
+}
