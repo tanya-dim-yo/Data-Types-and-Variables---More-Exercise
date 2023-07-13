111111d4682aa96a797a9dbdcca7d86bbb425457
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-13 03:55:21 +0300

    Add files via upload

---
+++ 04. Refactoring: Prime Checker/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= number; i++)
+{
+    int ctr = 0;
+
+    for (int j = 2; j <= i / 2; j++)
+    {
+        if (i % j == 0)
+        {
+            ctr++;
+            break;
+        }
+    }
+
+    if (ctr == 0)
+    {
+        Console.WriteLine($"{i} -> true");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> false");
+    }
+}
